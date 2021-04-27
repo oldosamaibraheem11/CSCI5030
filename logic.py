45de3e0fe3a52b9eb37a5f6888226cdccc4875ff
FaizanSyed95 <ahmed.faizaneng@gmail.com>
2021-04-26 20:58:11 -0500

    kwic method working

--- logic.py
+++ logic.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'CSCI5030@SLU2021'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'CSCI5030SLU2021'
 app.config['MYSQL_DATABASE_DB'] = 'wordsense'
 
 mysql = MySQL(app)
