7adfc38e1eca6497d717d291588c7e4ea0804a14
FaizanSyed95 <79019862+FaizanSyed95@users.noreply.github.com>
2021-05-09 13:46:52 -0500

    Update Kwic.py
    added new file

--- Kwic.py
+++ Kwic.py
@@ -1,67 +1,93 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Apr 23 01:11:10 2021
-
 @author: FaizanSyed
 """
-#pip install string-color
-
-from stringcolor import * 
 import logic
+import re
 
-def KW(keyindex, line_text ,Surrounding_word):    
+
+def KW(word_selected, line_text , surrounding_word, wordamount):
     
-    
-    #keyindex = "one/cd"
-    #line_text = logic.SQLQuery("select line_text from english_corpus where line_id in (2819, 4702, 4704, 5756, 5758, 5760, 5783, 5788, 5791, 5792, 5896, 5898, 5924, 5931, 18992);")
+ 
     sentencedic = dict()
-    #print(line_text)
     sentlist = []
-
+    
     for x in range(0,len(line_text)):
-        sentencedic[keyindex] = line_text[x]
-        wordlist = sentencedic[keyindex].split()
-    
-    "I have a Primary number two three"
+        sentencedic[word_selected] = line_text[x]
+        wordlist = sentencedic[word_selected].split()
+        
+    idx = 0
     for i in range(0,len(line_text)):
-    
-        if keyindex in line_text[i]:
-            result = line_text[i].index(keyindex)
-            split_text = line_text[i].split()
-            keywordindex = int(split_text.index(keyindex))
-            string = split_text
-         #   print(string)
+        if word_selected in line_text[i]:
+            result = line_text[i].index(word_selected)
+            sentence_split = re.findall(f"[\w']+|[.,!?;]", line_text[i])
+            for words in sentence_split:
+                if word_selected in words:
+                    keywordindex = sentence_split.index(words)
+            string = sentence_split
+            if wordamount == -1:
+                ngrams=[]
+                surrounding_word = len(string)
+                for i in range(len(string)-(surrounding_word-1)):
+                    ngrams.append(string[i:i+(surrounding_word)])
+
+                    if word_selected in ngrams[i]:
+                        keynumber = ngrams[i].index(word_selected)
+                kwicdict = {}
+
+                for n in ngrams:
+                    if n[keynumber] not in kwicdict:
+                        kwicdict[n[keynumber]] = [n]
+                    else:
+                        kwicdict[n[keynumber]].append(n)
+                try:
+                    count= 0 
+                    for n in kwicdict[word_selected]:
+
+                        outstring1 = ' '.join(n[:keynumber])
+                        outstring2 = "<b><span style='color:red;'>"+word_selected+"</b></span>"
+                        outstring3 = ' '.join(n[keynumber+1:])
+                        idx = idx+1
+                        table_row_num = str(idx)
+
+                        sentlist.append('<tr ><td style = padding-top:30px;>'+table_row_num + "."+ " &nbsp; " +outstring1+ " &nbsp; " + outstring2 + " &nbsp;"+outstring3+'</td></tr>  ')
+                        
+                except:
+                    pass
+            else:
+                ngrams=[]
+                for i in range(len(string)-(surrounding_word-1)):
+                    ngrams.append(string[i:i+(surrounding_word)])
+
+                keynumber = int((surrounding_word-1)/2)
+                kwicdict = {}
+                
+                for n in ngrams:
+                    if n[keynumber] not in kwicdict:
+                        kwicdict[n[keynumber]] = [n]
+                    else:
+                        kwicdict[n[keynumber]].append(n)
+                try:
+
+                    for n in kwicdict[word_selected]:
+                        outstring1 =' '.join(n[:keynumber])
+                        outstring2 = word_selected
+                        outstring3 = ' '.join(n[(keynumber+1):])
+                        idx = idx+1
+                        table_row_num = str(idx)
+                        sentlist.append('<tr><td>'+table_row_num+'</td>'+'<td style="text-align:right;">'+outstring1+'</td><td style="text-align:left; font-weight: bold;color: rgb(255,20,20);">'+ " &nbsp; " + outstring2 + " &nbsp;"+'</td><td style="text-align:left;">'+outstring3+'</td></tr>')
+                except:
+                    pass
         
-            ngrams = [string[i:i+((Surrounding_word*2)+1)] for i in range(len(string)-5)]
-           # print(string[i+5:i+10])
-            
-        
-            kwicdict = {}
-            for n in ngrams:
-                if keyindex not in kwicdict:
-                    kwicdict[keyindex] = [n]
-                else:
-                    kwicdict[keyindex].append(n)
-        
-            for n in kwicdict[keyindex]:
-                outstring = ' '.join(n[:Surrounding_word]).rjust(50)
-                outstring += str(cs( keyindex , "DarkViolet2" )).center(len(keyindex)+20)
-                outstring += ' '.join(n[Surrounding_word+1:])
-                sentlist.append(outstring)
-            #print(cs(outstring, "DarkViolet2", "lightgrey6"))
-            for sents in sentlist:
-                print(sents)
-#black/jj
-#one/cd
-#from/in
-#two/cd
-#with/in
-#into/in
-#the/at
-#of/in
+
+    return sentlist
 
 
+def KWlist(word_selected , biglist,surrounding_word,wordamount):
+    sentence_list_cluster=[]
 
+    for small_list in biglist:
+        new_list = KW(word_selected, small_list,surrounding_word,wordamount)
+        sentence_list_cluster.append(new_list)
 
-Kwic_check= KW("black/jj" ,logic.SQLQuery("select line_text from english_corpus where line_id in (2819, 4702, 4704, 5756, 5758, 5760, 5783, 5788, 5791, 5792, 5896, 5898, 5924, 5931, 18992);"),5)
-
+    return sentence_list_cluster 
